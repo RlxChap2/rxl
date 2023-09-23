75b0d037045eec79d81f472890742596aa4c3dd6
RlxChap2 <133292635+RlxChap2@users.noreply.github.com>
2023-09-23 23:20:48 +0300

    v1.0

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function rxl(string) {
-    return string === 'rxl'
+  return string === "rxl";
 }
 
-module.exports = rxl;
+async function reply(options = {}) {
+  if (!options.interaction) throw new TypeError("interaction is not definded");
+  if (!options.message) throw new TypeError("message in not definded");
+
+  await options.interaction.reply(options.message);
+  console.log(
+    options.message,
+    `UserID: ${options.interaction.user.id}`,
+    `Username: ${options.interaction.user.username}`
+  );
+}
+
+module.exports = { rxl, reply };
