c3c18a2ec79070368bc4d981ac70c4cb96a507e4
RlxChap2 <133292635+RlxChap2@users.noreply.github.com>
2023-11-16 01:03:16 +0300

    v1.1.2

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,4 +3,5 @@ export declare class Log {
 }
 export declare class Reply {
     static interaction(msg: string, int: any): Promise<void>;
+    static message(msg: string, int: any): Promise<void>;
 }
