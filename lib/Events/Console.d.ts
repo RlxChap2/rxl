5fedafe02007f0ce631a748cad83d37551d3ea7f
RlxChap2 <133292635+RlxChap2@users.noreply.github.com>
2023-11-22 07:21:58 +0300

    Copyright

--- lib/Events/Console.d.ts
+++ lib/Events/Console.d.ts
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2023-present, RlxChap2, rxl-npm
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 export declare const RColors: {
     White: string;
     Cyan: string;
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2023-present, RlxChap2, rxl-npm
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import { Reply } from "./Events/Interactions";
 import { Console, RColors } from "./Events/Console";
 export { Reply, Console, RColors };
